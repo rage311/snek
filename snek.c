c4abc5c914fbc85d235d6dbe8f30e0c0452a79c6
rage311 <rage_311@hotmail.com>
2016-12-31 11:07:41 -0700

    Added vim keybindings for movement. Added comments. Added score bar. Fixed some code formatting.

--- snek.c
+++ snek.c
@@ -31,7 +31,7 @@ void death(int length, body_segment_t *head)
 
   clear();
   mvaddstr(0, 0, "YOU DIED");
-  mvprintw(1, 0, "Score: %d", length - STARTING_SEGMENTS);
+  mvprintw(1, 0, "Score: %d\n", length - STARTING_SEGMENTS);
 
   refresh();
   sleep(1);
@@ -55,23 +55,25 @@ void place_fruit(int *fruit_xy, body_segment_t *head, int max_x, int max_y)
   body_segment_t *body_ptr;
 
   fruit_x = rand() % (max_x - 2) + 1;
-  fruit_y = rand() % (max_y - 2) + 1;
+  fruit_y = rand() % (max_y - 3) + 2;
 
   body_ptr = head;
 
+  /* Ensure fruit doesn't get placed on snake */
   while (body_ptr) {
     if (fruit_x == body_ptr->x && fruit_y == body_ptr->y) {
       fruit_x = rand() % (max_x - 2) + 1;
-      fruit_y = rand() % (max_y - 2) + 1;
+      fruit_y = rand() % (max_y - 3) + 2;
       body_ptr = head;
-    }
-    else {
+    } else {
       body_ptr = body_ptr->next;
     }
   }
-  
+
+  /* Draw fruit */
   mvaddstr(fruit_y, fruit_x, "@");
 
+  /* Copy values for "returned" array */
   fruit_xy[0] = fruit_x;
   fruit_xy[1] = fruit_y;
 
@@ -86,7 +88,7 @@ int main(int argc, char **argv)
   player_t player;
   int x_idx;
   int y_idx;
-  int add_one = 0;
+  unsigned char add_one = 0;
   body_segment_t *body_ptr;
   int next_x;
   int next_y;
@@ -94,7 +96,9 @@ int main(int argc, char **argv)
   int prev_y;
   int fruit_xy[2];
   int i;
+  char score[5] = { '0', '\0', '\0', '\0', '\0' };
 
+  /* Initialize curses and screen */
   initscr();
   noecho();
   curs_set(FALSE);
@@ -103,10 +107,12 @@ int main(int argc, char **argv)
 
   getmaxyx(stdscr, max_y, max_x);
 
+  /* Seed rand */
   srand(time(NULL));
 
+  /* Draw walls */
   for (x_idx = 0; x_idx < max_x; x_idx++) {
-    mvaddstr(0, x_idx, "#");
+    mvaddstr(1, x_idx, "#");
     mvaddstr(max_y - 1, x_idx, "#");
   }
   for (y_idx = 1; y_idx < max_y - 1; y_idx++) {
@@ -114,6 +120,9 @@ int main(int argc, char **argv)
     mvaddstr(y_idx, max_x - 1, "#");
   }
 
+  mvaddstr(0, 0, "Score: 0");
+
+  /* Initial snake values */
   player.length = STARTING_SEGMENTS;
   player.direction = RIGHT;
   player.head = (body_segment_t *) malloc(sizeof(body_segment_t));
@@ -125,8 +134,7 @@ int main(int argc, char **argv)
 
     if (i < STARTING_SEGMENTS - 1) {
       body_ptr->next = (body_segment_t *) malloc(sizeof(body_segment_t));
-    }
-    else {
+    } else {
       body_ptr->next = 0;
     }
 
@@ -137,50 +145,54 @@ int main(int argc, char **argv)
 
   place_fruit(fruit_xy, player.head, max_x, max_y);
 
-  while(1) {
-    while ((ch = getch()) != ERR) {
+  while (1) {
+    prev_x = player.head->x;
+    prev_y = player.head->y;
+
+    if ((ch = getch()) != ERR) {
       switch (ch) {
         case KEY_UP:
+        case 'k':
           player.direction = UP;
           break;
         case KEY_DOWN:
+        case 'j':
           player.direction = DOWN;
           break;
         case KEY_LEFT:
+        case 'h':
           player.direction = LEFT;
           break;
         case KEY_RIGHT:
+        case 'l':
           player.direction = RIGHT;
           break;
       }
     }
 
-    prev_x = player.head->x;
-    prev_y = player.head->y;
-
     switch (player.direction) {
       case UP:
-        prev_y -= 1;
+        --prev_y;
         break;
       case DOWN:
-        prev_y += 1;
+        ++prev_y;
         break;
       case LEFT:
-        prev_x -= 1;
+        --prev_x;
         break;
       case RIGHT:
-        prev_x += 1;
+        ++prev_x;
         break;
     }
 
-    if (prev_y == 0 || prev_x == 0 ||
+    /* Check if we've hit a wall */
+    if (prev_y <= 1 || prev_x == 0 ||
         prev_y == max_y - 1 || prev_x == max_x - 1) {
       death(player.length, player.head);
       player.head = NULL;
       body_ptr = NULL;
       exit(EXIT_SUCCESS);
-    }
-    else if (prev_y == fruit_xy[1] && prev_x == fruit_xy[0]) {
+    } else if (prev_y == fruit_xy[1] && prev_x == fruit_xy[0]) {
       add_one = 1;
     }
 
@@ -193,6 +205,7 @@ int main(int argc, char **argv)
       body_ptr->x = prev_x;
       body_ptr->y = prev_y;
 
+      /* If the head runs into the body */
       if (body_ptr != player.head &&
           body_ptr->x == player.head->x &&
           body_ptr->y == player.head->y) {
@@ -205,15 +218,19 @@ int main(int argc, char **argv)
       prev_x = next_x;
       prev_y = next_y;
 
+      /* Draw current position as part of the snake */
       mvaddstr(body_ptr->y, body_ptr->x, "%");
 
+      /* If we're at the end of the tail and need to extend the length */
       if (!body_ptr->next && add_one) {
         body_ptr->next = (body_segment_t *) malloc(sizeof(body_segment_t));
         body_ptr->next->x = prev_x;
         body_ptr->next->y = prev_y;
         body_ptr->next->next = 0;
 
-        player.length += 1;
+        ++player.length;
+        snprintf(score, 5, "%d", player.length - STARTING_SEGMENTS);
+        mvaddstr(0, 7, score);
         place_fruit(fruit_xy, player.head, max_x, max_y);
         add_one = 0;
       }
@@ -221,16 +238,21 @@ int main(int argc, char **argv)
       body_ptr = body_ptr->next;
     }
 
+    /* Blank out previous tail end position unless that's where the new
+     * fruit is or where the head is moving to */
     if ((player.head->x != prev_x || player.head->y != prev_y) &&
         (prev_y != fruit_xy[1] || prev_x != fruit_xy[0])) {
       mvaddstr(prev_y, prev_x, " ");
     }
 
     refresh();
+
+    /* Sleep before next cycle
+     * Sleep longer if going up or down, since terminal characters are taller
+     * than they are wide and we want the speed to feel the same */
     if (player.direction == UP || player.direction == DOWN) {
-      usleep(150000);
-    }
-    else {
+      usleep(160000);
+    } else {
       usleep(100000);
     }
   }
